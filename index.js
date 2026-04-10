94eb59faa767b13b5ebb20505a499e9634973d2d
allexmelo13-del <allexmelo13@gmail.com>
2026-04-10 17:29:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ async function start() {
 
   const browser = await puppeteer.launch({
   headless: true,
-  executablePath: "/opt/render/.cache/puppeteer/chrome/linux-127.0.6533.88/chrome-linux64/chrome",
-  args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  args: ["--no-sandbox", "--disable-setuid-sandbox"],
 });
 
   const page = await browser.newPage();
